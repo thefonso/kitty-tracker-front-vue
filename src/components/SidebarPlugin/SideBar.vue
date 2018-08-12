3221fb0722fd7f32f4a8ae6b40b4ece786e2130a
thefonso <thefonso@thefonso.com>
2018-08-12 12:48:30 -0500

    removed unneeded sidebar menu items for iphone layout

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -54,6 +54,7 @@
       },
       backgroundImage: {
         type: String,
+        // TODO: change to babby image
         default: 'static/img/sidebar-5.jpg'
       },
       activeColor: {
--- src/pages/Dashboard/Layout/Extra/MobileMenu.vue
+++ src/pages/Dashboard/Layout/Extra/MobileMenu.vue
@@ -1,55 +1,55 @@
 <template>
-  <ul class="nav nav-mobile-menu">
-    <form class="navbar-form navbar-left navbar-search-form" role="search">
-      <div class="input-group">
-        <i class="nc-icon nc-zoom-split"></i>
-        <input type="text" value="" class="form-control" placeholder="Search...">
-      </div>
-    </form>
-    <drop-down>
-      <template slot="title">
-        <i class="nc-icon nc-planet"></i>
-      </template>
-      <a class="dropdown-item" href="#">Create New Post</a>
-      <a class="dropdown-item" href="#">Manage Something</a>
-      <a class="dropdown-item" href="#">Do Nothing</a>
-      <a class="dropdown-item" href="#">Submit to Live</a>
-      <a class="dropdown-item" href="#">Another action</a>
-    </drop-down>
-    <drop-down>
-      <template slot="title">
-        <i class="nc-icon nc-bell-55"></i>
-        <span class="notification">5</span>
-        <span class="d-lg-none">Notification</span>
-      </template>
-      <a class="dropdown-item" href="#">Notification 1</a>
-      <a class="dropdown-item" href="#">Notification 2</a>
-      <a class="dropdown-item" href="#">Notification 3</a>
-      <a class="dropdown-item" href="#">Notification 4</a>
-      <a class="dropdown-item" href="#">Another notification</a>
-    </drop-down>
+  <!--<ul class="nav nav-mobile-menu">-->
+    <!--<form class="navbar-form navbar-left navbar-search-form" role="search">-->
+      <!--<div class="input-group">-->
+        <!--<i class="nc-icon nc-zoom-split"></i>-->
+        <!--<input type="text" value="" class="form-control" placeholder="Search...">-->
+      <!--</div>-->
+    <!--</form>-->
+    <!--<drop-down>-->
+      <!--<template slot="title">-->
+        <!--<i class="nc-icon nc-planet"></i>-->
+      <!--</template>-->
+      <!--<a class="dropdown-item" href="#">Create New Post</a>-->
+      <!--<a class="dropdown-item" href="#">Manage Something</a>-->
+      <!--<a class="dropdown-item" href="#">Do Nothing</a>-->
+      <!--<a class="dropdown-item" href="#">Submit to Live</a>-->
+      <!--<a class="dropdown-item" href="#">Another action</a>-->
+    <!--</drop-down>-->
+    <!--&lt;!&ndash;<drop-down>&ndash;&gt;-->
+      <!--&lt;!&ndash;<template slot="title">&ndash;&gt;-->
+        <!--&lt;!&ndash;<i class="nc-icon nc-bell-55"></i>&ndash;&gt;-->
+        <!--&lt;!&ndash;<span class="notification">5</span>&ndash;&gt;-->
+        <!--&lt;!&ndash;<span class="d-lg-none">Notification</span>&ndash;&gt;-->
+      <!--&lt;!&ndash;</template>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">Notification 1</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">Notification 2</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">Notification 3</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">Notification 4</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">Another notification</a>&ndash;&gt;-->
+    <!--&lt;!&ndash;</drop-down>&ndash;&gt;-->
 
-    <drop-down>
-      <template slot="title">
-        <i class="nc-icon nc-bullet-list-67"></i>
-      </template>
-      <a class="dropdown-item" href="#">
-        <i class="nc-icon nc-umbrella-13"></i> Help Center
-      </a>
-      <a class="dropdown-item" href="#">
-        <i class="nc-icon nc-email-85"></i> Messages
-      </a><a class="dropdown-item" href="#">
-      <i class="nc-icon nc-settings-90"></i> Settings
-    </a>
-      <div class="divider"></div>
-      <a class="dropdown-item" href="#">
-        <i class="nc-icon nc-lock-circle-open"></i> Lock Screen
-      </a>
-      <a href="#" class="dropdown-item text-danger">
-        <i class="nc-icon nc-button-power"></i> Log out
-      </a>
-    </drop-down>
-  </ul>
+    <!--&lt;!&ndash;<drop-down>&ndash;&gt;-->
+      <!--&lt;!&ndash;<template slot="title">&ndash;&gt;-->
+        <!--&lt;!&ndash;<i class="nc-icon nc-bullet-list-67"></i>&ndash;&gt;-->
+      <!--&lt;!&ndash;</template>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">&ndash;&gt;-->
+        <!--&lt;!&ndash;<i class="nc-icon nc-umbrella-13"></i> Help Center&ndash;&gt;-->
+      <!--&lt;!&ndash;</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">&ndash;&gt;-->
+        <!--&lt;!&ndash;<i class="nc-icon nc-email-85"></i> Messages&ndash;&gt;-->
+      <!--&lt;!&ndash;</a><a class="dropdown-item" href="#">&ndash;&gt;-->
+      <!--&lt;!&ndash;<i class="nc-icon nc-settings-90"></i> Settings&ndash;&gt;-->
+    <!--&lt;!&ndash;</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<div class="divider"></div>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a class="dropdown-item" href="#">&ndash;&gt;-->
+        <!--&lt;!&ndash;<i class="nc-icon nc-lock-circle-open"></i> Lock Screen&ndash;&gt;-->
+      <!--&lt;!&ndash;</a>&ndash;&gt;-->
+      <!--&lt;!&ndash;<a href="#" class="dropdown-item text-danger">&ndash;&gt;-->
+        <!--&lt;!&ndash;<i class="nc-icon nc-button-power"></i> Log out&ndash;&gt;-->
+      <!--&lt;!&ndash;</a>&ndash;&gt;-->
+    <!--&lt;!&ndash;</drop-down>&ndash;&gt;-->
+  <!--</ul>-->
 </template>
 <script>
   import DropDown from 'src/components/Dropdown.vue'
